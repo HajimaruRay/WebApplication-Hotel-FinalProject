9bf08b021080c8d3dcfff328eb4c11870ecd1fac
HAJIMARU <144349636+HajimaruRay@users.noreply.github.com>
2025-04-08 11:39:35 +0700

    upload index and css

---
+++ index/index-Content.php
@@ -0,0 +1,17 @@
+<div class="Content">
+        <div class="slider">
+            <div class="slides">
+                <img src="/Image/รูปบรรยากาศ/oneHotel.png" alt="Image 1">
+                <img src="/Image/รูปบรรยากาศ/twoHotel.png" alt="Image 2">
+                <img src="/Image/รูปบรรยากาศ/threeHotel.png" alt="Image 3">
+            </div>
+
+            <button class="prev" onclick="moveSlide(-1)">&#10094;</button>
+            <button class="next" onclick="moveSlide(1)">&#10095;</button>
+        </div>
+        <hr class="TheLine">
+        <div class="Greeting">
+            <p class="GreetingText">ยินดีต้อนรับสู่!<br>โรงเเรมชิมิจุ๊</p>
+            <a href="../php/booking/booking.php" class="BookingButton" style="text-decoration: none;">จองเลย!</a>
+        </div>
+    </div>
---
+++ index/index-ContentEN.php
@@ -0,0 +1,17 @@
+<div class="ContentEN">
+    <div class="slider">
+        <div class="slides">
+            <img src="/Image/รูปบรรยากาศ/oneHotel.png" alt="Image 1">
+            <img src="/Image/รูปบรรยากาศ/twoHotel.png" alt="Image 2">
+            <img src="/Image/รูปบรรยากาศ/threeHotel.png" alt="Image 3">
+        </div>
+
+        <button class="prev" onclick="moveSlideEN(-1)">&#10094;</button>
+        <button class="next" onclick="moveSlideEN(1)">&#10095;</button>
+    </div>
+    <hr class="TheLine">
+    <div class="Greeting">
+        <p class="GreetingText">Welcome to<br>CHIMIKU hotel</p>
+        <a href="../php/booking/booking.php" class="BookingButton" style="text-decoration: none;">Booking NOW!</a>
+    </div>
+</div>
